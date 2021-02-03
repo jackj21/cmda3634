484e8fcb98e75b00c909880f42ebe6e801c5e91b
Jack Jiang <jackj21@vt.edu>
2021-02-03 17:02:19 -0500

    First practice exercise with c (vim and cli)

---
+++ scratch/InClassExercise1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+printf("Name: Jack Jiang \n");
+printf("Major: CMDA \n");
+printf("Minor: CS Math \n");
+char grad[10]  = "Fall 2021";
+printf("Graduation Date: %s", grad);
+return 0;
+ }
+
