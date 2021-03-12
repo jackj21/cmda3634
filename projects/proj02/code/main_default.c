87a555bc471b5f32d2746158b7091ce20fc37eb2
Jack Jiang <jackj21@vt.edu>
2021-03-12 01:56:49 -0500

    Fixed parsing command line args with sscanf

--- projects/proj02/code/main_default.c
+++ projects/proj02/code/main_default.c
@@ -12,11 +12,17 @@ int main(int argc, char* argv[]) {
 
 	printf("Running %s...", argv[0]);
 
-	unsigned int n = argv[1];
-	unsigned int m_x = argv[2];
-	unsigned int m_y = argv[3];
-	float alpha = argv[4];
-	unsigned int T = argv[5];
-	
+	unsigned int n;
+	unsigned int m_x;
+	unsigned int m_y;
+	float alpha;
+	unsigned int T;
+
+	sscanf(argv[1], "%d", &n);
+	sscanf(argv[2], "%d", &m_x);
+	sscanf(argv[3], "%d", &m_y);
+	sscanf(argv[4], "%f", &alpha);
+	sscanf(argv[5], "%d", &T);
+
 	simulate(T, n, n, m_x, m_y);
 }
